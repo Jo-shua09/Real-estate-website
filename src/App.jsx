d848792f8add6dbd9778c4ee525d2538186c8573
Joshua Onyeka <joshuaonyeka039@gmail.com>
2024-12-26 23:12:21 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-xl font-extrabold underline">Hello all!</h1>
+      <h1 className="text-xl font-extrabold underline">Hello world!</h1>
     </div>
   );
 }
