1393e5066b3be2ab566e01c17d22ebff1af1aef6
Joshua Onyeka <joshuaonyeka039@gmail.com>
2024-12-29 15:38:39 +0100

    commit 4

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div className="container flex flex-col items-center justify-center mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden" id="about">
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+        About <span className="underline underline-offset-4 decoration-1 under font-light">our brand</span>
+      </h1>
+      <p className="text-gray-500 max-w-80 text-center mb-8 ">Passionate About Properties, Dedicated to Your Vision</p>
+    </div>
+  );
+};
+
+export default About;
