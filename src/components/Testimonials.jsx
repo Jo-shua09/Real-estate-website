a1dc4dd7589cce2e8858a8439d2fa6f98243d724
Joshua Onyeka <joshuaonyeka039@gmail.com>
2025-01-02 17:21:45 +0100

    commmit 6

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,3 +1,5 @@
+import { assets, testimonialsData } from "../assets/assets";
+
 const Testimonials = () => {
   return (
     <div className="container flex flex-col justify-center mx-auto py-4 pt-20 md:px-20 lg:px-32 w-full overflow-hidden" id="testimonials">
@@ -6,7 +8,24 @@ const Testimonials = () => {
       </h1>
       <p className="text-gray-500 max-w-80 text-center mx-auto mb-8 normal-case">Real Stories from Those Who Found Home with Us</p>
 
-      <div className=""></div>
+      <div className="flex flex-wrap justify-center gap-8">
+        {testimonialsData.map((testimonial, index) => (
+          <div key={index} className="max-w-[340px] border shadow-lg rounded px-8 py-12 text-center">
+            <img src={testimonial.image} alt={testimonial.alt} className="w-20 h-20 rounded-full mx-auto mb-4" />
+            <h2 className="text-xl text-gray-700 font-medium">{testimonial.name}</h2>
+            <p className="text-gray-500 mb-4 text-sm">{testimonial.title}</p>
+            <div
+              className="flex justify-center gap-1 textred500
+             mb-4"
+            >
+              {Array.from({ length: testimonial.rating }, (item, index) => (
+                <img key={index} src={assets.star_icon} alt="" />
+              ))}
+            </div>
+            <p className="text-gray-600 normal-case">{testimonial.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
