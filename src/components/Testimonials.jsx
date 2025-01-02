8c0effc5e33e7b4ebdb019791fc0a4d89626bc67
Joshua Onyeka <joshuaonyeka039@gmail.com>
2025-01-02 17:04:39 +0100

    commit 5

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,14 @@
+const Testimonials = () => {
+  return (
+    <div className="container flex flex-col justify-center mx-auto py-4 pt-20 md:px-20 lg:px-32 w-full overflow-hidden" id="projects">
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        customers <span className="underline underline-offset-4 decoration-1 under font-light"> Testimonials</span>
+      </h1>
+      <p className="text-gray-500 max-w-80 text-center mx-auto mb-8 normal-case">Real Stories from Those Who Found Home with Us</p>
+
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Testimonials;
